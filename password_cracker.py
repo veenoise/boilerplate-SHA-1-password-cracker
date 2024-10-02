ccc5fdc6da8cd4f7921bb9892a0e495a5ce9dfe5
veenoise <william.eduard123@gmail.com>
2024-10-02 16:27:38 +0800

    Added the solution

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,22 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts = False):
-    return True
+    with open("top-10000-passwords.txt") as passwords:
+        lines_password = [line.strip() for line in passwords]
+
+        if not use_salts:
+            for i in lines_password:
+                if hashlib.sha1(i.encode()).hexdigest() == hash:
+                    return i
+            
+        else:
+            with open("known-salts.txt") as salts:
+                lines_salt = [line.strip() for line in salts]
+                for i in lines_password:
+                    for j in lines_salt:
+                        if hashlib.sha1(f"{i}{j}".encode()).hexdigest() == hash:
+                            return i
+                        elif hashlib.sha1(f"{j}{i}".encode()).hexdigest() == hash:
+                            return i
+
+    return "PASSWORD NOT IN DATABASE"
